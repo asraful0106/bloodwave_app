8ce03d1502b29edf145f4173a6eae7ed0986f2d7
asraful0106 <asrafulapf@gmail.com>
2026-02-20 17:48:07 +0600

    given, home, settings, profile page done!

---
+++ Pages/given/Given.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Given() {
+  return (
+    <View>
+      <Text>Given</Text>
+    </View>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
 import SafeScreen from "@/components/SafeScreen";
 import ForgotPassword from "@/Pages/auth/forgetPassword/ForgotPassword";
-import Login from "@/Pages/auth/login/Login";
-import OtpVerification from "@/Pages/auth/otp_varification/OtpVerification";
-import Register from "@/Pages/auth/registration/Registration";
-import ResetPassword from "@/Pages/auth/resetPassword/ResetPassword";
 
 export default function Index() {
   return (
